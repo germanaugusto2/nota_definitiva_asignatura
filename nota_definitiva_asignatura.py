f13f36a21411e2f82dbdf209d44f967301a0a694
jennyromero <jennyromero@colegioguanenta.edu.co>
2023-02-16 17:34:53 -0500

    Se creo el diagrama de flujo y se agrego al README

---
+++ nota_definitiva_asignatura.py
@@ -0,0 +1,13 @@
+# programa para realizar los operadores, usando las diferentes formulas 
+
+print("---------------------------------------")
+print("--el resultado de las operaciones son:--)
+print("---------------------------------------")
+# input 
+nc=int(input("digite el valor de procedimental"))
+np=int(input("digite el valor de cognitivo"))
+na=int(input("digite el valor de actitudinal"))
+au=int(input("digite el valor de auto evaluacion"))
+bi=int(input("digite el valor de bimestral"))
+# procesing 
+nd = (0.3*nc) + (0.1*na) + (0.1*au) + (0.2*bi)
